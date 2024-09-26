8c9d98638fb50e631006de4ee5635698cf0788d7
Luan <kenzleydev@outlook.com>
2024-09-26 11:39:48 -0300

    subindo estruturas iniciais

---
+++ src/test/java/br/com/health/MedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
